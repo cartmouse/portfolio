b5033004cac596ef9fc153d786cecc2135dee9ff
Tom Cartwright <cartmouse@outlook.com>
2023-11-09 12:38:19 +0000

    Remove profile image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,6 @@ import GitHub from "./github.svg";
 import LinkedIn from "./linkedin.svg";
 import Email from "./email.svg";
 import EviesButtons from "./eviesbuttons.png";
-import Profile from "./profile.jpg";
 import Widgets from "./widgets.png";
 import Madronzio from "./madronzio.webm";
 import Ballpit from "./ballpit.webm";
@@ -14,7 +13,6 @@ export {
   LinkedIn,
   Email,
   EviesButtons,
-  Profile,
   Widgets,
   Madronzio,
   Ballpit,
